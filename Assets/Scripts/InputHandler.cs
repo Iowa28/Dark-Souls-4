137b83ab8f8a18f43bcf74ddcd6640eaff60fba3
Niaz <yazzzik@list.ru>
2023-04-23 19:01:52 +0300

    init project

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace DS
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        private PlayerControls inputActions;
+
+        private Vector2 movementInput;
+        private Vector2 cameraInput;
+
+        private void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += input => movementInput = input.ReadValue<Vector2>();
+                inputActions.PlayerMovement.Camera.performed += input => cameraInput = input.ReadValue<Vector2>();
+            }
+            
+            inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+        }
+
+        private void MoveInput(float delta)
+        {
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace DS
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        private Transform cameraObject;
+        private InputHandler inputHandler;
+        private Vector3 moveDirection;
+
+        private new Rigidbody rigidbody;
+        private GameObject normalCamera;
+
+        [Header("Stats")] 
+        [SerializeField] 
+        private float movementSpeed = 5;
+
+        [SerializeField]
+        private float rotationSpeed = 10;
+        
+        void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            cameraObject = GameObject.FindWithTag("MainCamera").transform;
+        }
+
+        void Update()
+        {
+            inputHandler.TickInput(Time.deltaTime);
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+            moveDirection *= movementSpeed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelocity;
+        }
+
+        #region Movement
+
+        private Vector3 normalVector;
+        private Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            float moveOverride = inputHandler.moveAmount;
+
+            Vector3 targetDirection = cameraObject.forward * inputHandler.vertical;
+            targetDirection += cameraObject.right * inputHandler.horizontal;
+            
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if (targetDirection == Vector3.zero)
+            {
+                targetDirection = transform.forward;
+            }
+
+            Quaternion quaternion = Quaternion.LookRotation(targetDirection);
+            Quaternion targetRotation = Quaternion.Slerp(transform.rotation, quaternion, rotationSpeed * delta);
+
+            transform.rotation = targetRotation;
+        }
+
+        #endregion
+        
+    }
+}
